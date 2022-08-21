7d33ccbbf152344d7b4b55c69a18c08b97abdc28
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-08-21 00:51:16 -0300

    Adicionando CSS e novas funcoes

--- jogoTeca.py
+++ jogoTeca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 class Game:
     def __init__(self,name,category,console):
@@ -6,16 +6,17 @@ class Game:
         self.category = category
         self.console = console
 
+game1 = Game("Tetris","Puzzle","Atari")
+game2 = Game("God of War","Hack n Alash","PS2")
+game3 = Game("Mortal Kombat","Luta","PS2")
+listGames = [game1,game2,game3]
+
+
 app = Flask(__name__) #RESPONSAVEL POR CRIAR O SERVIDOR ONDE RODA A APLICACAO NA WEB
 
-@app.route('/inicio') #responsavel pela criacao do "caminho" na URL
-def carregaGames():
-    game1 = Game("Tetris","Puzzle","Atari")
-    game2 = Game("God of War","Hack n Alash","PS2")
-    game3 = Game("Mortal Kombat","Luta","PS2")
-    listGames = [game1,game2,game3]
+@app.route('/') #responsavel pela criacao do "caminho" na URL
+def index():
     titulo = "GAMES"
-
     return render_template('games.html',titulo = titulo,listGames = listGames)
 
 @app.route('/newGame')
@@ -23,4 +24,16 @@ def newGame():
     titulo = "Cadastro de Novos GAMES"
     return render_template('newGame.html',titulo = titulo)
 
-app.run() #RODA A APLICACAO
+
+@app.route('/create',methods=['POST',])
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+
+    game = Game(name,category,console)
+    listGames.append(game)
+
+    return redirect('/')
+
+app.run(debug=True) #RODA A APLICACAO
